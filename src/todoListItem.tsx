833e4e10d85af17e7a606bb2570e0c0a7b83c737
poppinkaz <kazuki-suzuki@keio.jp>
2021-02-28 09:15:54 +0000

    part1 mid-progress

---
+++ src/todoListItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface TodoListItemProps{
+    todo: Todo;
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = ({todo}) => {
+    return (
+      <li>
+        <label style={{textDecoration: todo.complete? "line-through": "none"}}>
+
+            <input type="checkbox" checked={todo.complete}/>
+            {todo.text}
+
+        </label>
+      </li>
+    );
+};
